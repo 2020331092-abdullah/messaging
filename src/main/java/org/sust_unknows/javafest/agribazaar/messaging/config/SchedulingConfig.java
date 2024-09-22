f02d5413b1bdf6720661a3380cbb4d35ee1fbb7a
2020331092-abdullah <114574061+2020331092-abdullah@users.noreply.github.com>
2024-09-22 19:20:04 +0600

    Delete src/main/java/org/sust_unknows/javafest/agribazaar/messaging directory

--- src/main/java/org/sust_unknows/javafest/agribazaar/messaging/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package org.sust_unknows.javafest.agribazaar.messaging.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {
-}
